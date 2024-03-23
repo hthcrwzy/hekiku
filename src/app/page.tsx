b5376525d9ce7c8076f49d435ad93f94185e4b79
Yamizo <119731650+hthcrwzy@users.noreply.github.com>
2024-03-23 14:55:58 +0900

    Finish making home and reading view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { NotoSansJPLocal } from "./font";
 export default function Home() {
   return (
     <main className={NotoSansJPLocal.className}>
-      <div className="flex flex-col space-y-4">
-        <header className="flex flex-col justify-center space-y-4 text-center h-96 bg-gradient-to-r from-cyan-500 to-blue-500">
+      <div className="bg-gradient-to-b from-sky-200 dark:bg-gradient-to-b dark:from-sky-600 dark:to-slate-900 min-h-screen flex flex-col space-y-4 text-black dark:text-white">
+        <header className="flex flex-col justify-center space-y-4 text-center h-96">
           <h1 className="font-extrabold text-5xl">Welcome to Hekiku!</h1>
           <h2 className="opacity-75">AOZORA BUNKO READER</h2>
         </header>
-        <Link href="reader">Open and Read</Link>
+        <Link href="reader" className="justify-center text-center">
+          Open and Read
+        </Link>
       </div>
     </main>
   );
--- src/app/reader/page.tsx
+++ src/app/reader/page.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
 import Open from "../open";
-import styles from "./styles.module.css";
 
 export default function Page() {
   return (
-    <section className="flex flex-col items-center p-24 h-screen">
+    <section className="bg-white dark:bg-slate-800 flex flex-col items-center p-24 h-screen">
       <h1>Reader</h1>
       <Link href="/">Back to home</Link>
       <Open />
